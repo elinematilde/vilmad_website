a5ab56e11e9f3aa4ff3e56ab2540c2c2bd282d35
Joris <jote0002@stud.kea.dk>
2024-02-15 15:59:03 +0100

    tilføjet råkode og linket til databasen

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-s;
+//https://pkjbphmcnorwfmrlmfci.supabase.co
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBramJwaG1jbm9yd2ZtcmxtZmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3MjUyNzQsImV4cCI6MjAyMzMwMTI3NH0.nLeOP6ZBdwY2RvoWnk5x8a-WlwRSbbsoQKcrRkSCBHI
+
+fetch("https://pkjbphmcnorwfmrlmfci.supabase.co/rest/v1/VildMadData?", {
+  method: "GET",
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBramJwaG1jbm9yd2ZtcmxtZmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3MjUyNzQsImV4cCI6MjAyMzMwMTI3NH0.nLeOP6ZBdwY2RvoWnk5x8a-WlwRSbbsoQKcrRkSCBHI",
+  },
+})
+  .then((res) => res.json())
+  .then(showData);
+
+function showData(data) {
+  // We have the data
+  console.log(data);
+  console.log("heeey");
+  //data.forEach();
+}
+
+//function (){}
+
+function test() {
+  console.log("test");
+}
