044b2f487ede3668a962d4561a72d812c6a01864
Joris <jote0002@stud.kea.dk>
2024-02-16 10:29:23 +0100

    råkodet produktliste, uden filtrering

--- main.js
+++ main.js
@@ -15,11 +15,37 @@ function showData(data) {
   // We have the data
   console.log(data);
   console.log("heeey");
-  //data.forEach();
+  data.forEach(listMushroom);
 }
 
 //function (){}
 
 function test() {
   console.log("test");
+}
+
+function listMushroom(oneMushroom) {
+  console.log("listMushroom");
+  const product = document.querySelector("template").content;
+
+  // Make a copy
+  const myClone = product.cloneNode(true);
+
+  // Change content
+  myClone.querySelector(".title").textContent = oneMushroom.title;
+  //myClone.querySelector(".card-content").textContent = oneMushroom.season_0;
+
+  myClone.querySelector(".mush-img").src = `${oneMushroom.mush_img}`;
+  console.log(oneMushroom.id);
+
+  myClone.querySelector(".mushroom-card").setAttribute("href", `produkt.html?id=${oneMushroom.id}`);
+
+  //if (((oneMushroom.season_0 = 0), 1, 2, 3)) {
+  //  console.log(oneMushroom.season_0);
+  // }
+
+  // Append
+  console.log("paste");
+  const parentElement = document.querySelector(".produktliste");
+  parentElement.appendChild(myClone);
 }
