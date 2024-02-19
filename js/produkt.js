159931d8752b91ccffc14a9c50a54603922f6068
Joris <jote0002@stud.kea.dk>
2024-02-19 10:39:06 +0100

    Flyttet mapper i master

---
+++ js/produkt.js
@@ -0,0 +1,63 @@
+// const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get("id");
+
+// const url = "https://pkjbphmcnorwfmrlmfci.supabase.co/rest/v1/VildMadData?" + id;
+
+// fetch(url, {
+//   method: "GET",
+//   headers: {
+//     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBramJwaG1jbm9yd2ZtcmxtZmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3MjUyNzQsImV4cCI6MjAyMzMwMTI3NH0.nLeOP6ZBdwY2RvoWnk5x8a-WlwRSbbsoQKcrRkSCBHI",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then(showMush);
+
+// function showMush() {
+//   console.log(id);
+//   showMushroom();
+// }
+// function showMush(mush) {
+//   console.log(mush.id);
+//   document.querySelector("h2").textContent = mush.id;
+// }
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const url = `https://pkjbphmcnorwfmrlmfci.supabase.co/rest/v1/VildMadData?id=eq.${id}`;
+
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBramJwaG1jbm9yd2ZtcmxtZmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3MjUyNzQsImV4cCI6MjAyMzMwMTI3NH0.nLeOP6ZBdwY2RvoWnk5x8a-WlwRSbbsoQKcrRkSCBHI"; // Replace "YOUR_API_KEY" with your actual API key
+
+fetch(url, {
+  headers: {
+    apikey: apiKey,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(data) {
+  console.log(data);
+
+  const oneMushroom = data[0];
+
+  document.querySelector("h1").textContent = oneMushroom.title;
+  document.querySelector(".showId").textContent = oneMushroom.id;
+  document.querySelector(".mushSeason").textContent = oneMushroom.seasons;
+  document.querySelector(".img1").src = `${oneMushroom.mush_img}`;
+  document.querySelector(".description").textContent = oneMushroom.description;
+  document.querySelector(".harvestingLandscape").textContent = oneMushroom.harvesting_landscapes;
+  document.querySelector(".headCatagory").textContent = oneMushroom.head_category;
+  document.querySelector(".img2").src = `${oneMushroom.landscape_img}`;
+
+  //document.querySelector(".goBackLink").setAttribute("href", `sub-produktliste.html?seasons=${oneMushroom.seasons[0]}`);
+
+  if ((oneMushroom.season_1 = 5)) {
+    console.log("June");
+  }
+}
+
+function goBack() {
+  console.log("back");
+  window.history.back();
+}
---
+++ js/subpage.js
@@ -0,0 +1,52 @@
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get("seasons");
+
+const url = `https://pkjbphmcnorwfmrlmfci.supabase.co/rest/v1/VildMadData?seasons=cs.["${query}"]`;
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBramJwaG1jbm9yd2ZtcmxtZmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3MjUyNzQsImV4cCI6MjAyMzMwMTI3NH0.nLeOP6ZBdwY2RvoWnk5x8a-WlwRSbbsoQKcrRkSCBHI";
+
+fetch(url, {
+  headers: {
+    apikey: apiKey,
+  },
+})
+  .then((response) => response.json())
+  .then(showMushrooms);
+
+function showMushrooms(data) {
+  data.forEach((element) => {
+    //Fra YT tutorial
+    // const link = document.createElement("a");
+    //link.href = `produkt.html?id=${element.id}`;
+    //link.textContent = element.title;
+
+    const product = document.querySelector("template").content;
+
+    // Make a copy
+    const myClone = product.cloneNode(true);
+
+    // Change content
+    myClone.querySelector(".title").textContent = element.title;
+    //myClone.querySelector(".card-content").textContent = oneMushroom.season_0;
+
+    myClone.querySelector(".mush-img").src = `${element.mush_img}`;
+    console.log(element.id);
+
+    const urlParamsCat = new URLSearchParams(window.location.search);
+    const id = urlParamsCat.get("id");
+
+    myClone.querySelector(".mushroom-card").setAttribute("href", `produkt.html?id=${element.id}`);
+
+    // Append
+    console.log("append");
+    const parentElement = document.querySelector(".produktliste");
+    parentElement.appendChild(myClone);
+
+    //fra yt tutorial
+    // document.body.appendChild(link);
+  });
+}
+
+function goBack() {
+  console.log("back");
+  window.history.back();
+}
