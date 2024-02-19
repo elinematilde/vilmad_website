e7560c0592ff841ed0cc52e95f35fa798f6d18d9
Joris <jote0002@stud.kea.dk>
2024-02-19 13:53:36 +0100

    fixet filtrerings titel og parametre

--- js/subpage.js
+++ js/subpage.js
@@ -1,7 +1,18 @@
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get("seasons");
+const landscapes = urlParams.get("harvesting_landscapes");
+let urlmodifier = "";
+let title = "";
 
-const url = `https://pkjbphmcnorwfmrlmfci.supabase.co/rest/v1/VildMadData?seasons=cs.["${query}"]`;
+if (query) {
+  urlmodifier = `?seasons=cs.["${query}"]`;
+  title = query;
+} else if (landscapes) {
+  urlmodifier = `?harvesting_landscapes=eq.${landscapes}`;
+  title = landscapes;
+}
+
+const url = `https://pkjbphmcnorwfmrlmfci.supabase.co/rest/v1/VildMadData${urlmodifier}`;
 const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBramJwaG1jbm9yd2ZtcmxtZmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3MjUyNzQsImV4cCI6MjAyMzMwMTI3NH0.nLeOP6ZBdwY2RvoWnk5x8a-WlwRSbbsoQKcrRkSCBHI";
 
 fetch(url, {
@@ -13,7 +24,7 @@ fetch(url, {
   .then(showMushrooms);
 
 function showMushrooms(data) {
-  document.querySelector(".subCategory").textContent = query;
+  document.querySelector(".subCategory").textContent = title;
 
   data.forEach((element) => {
     //Fra YT tutorial
